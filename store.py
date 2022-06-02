d0ae8941850bf1d181df7b428b3e6cb4c8780c47
Jason Roman <funjasonroman@gmail.com>
2022-06-01 19:45:34 -0600

    You can buy materials from the shop

--- store.py
+++ store.py
@@ -9,10 +9,20 @@ while True:
 		u = ui.UI()
 		u.add(ui.Header("Market"))
 		u.add(ui.Text(f"You have ${itemhelpers.getMoney()}"))
+		# Selling
 		sellfunc = lambda mat: (lambda: itemhelpers.loseItem(mat) or itemhelpers.setMoney(itemhelpers.getMoney() + 1))
 		for mat in [["Wood", "wood"], ["Leaves", "leaves"], ["Blossoms", "blossom"], ["Apples", "apple"], ["Berries", "berry"]]:
-			if itemhelpers.getItems()[mat[1]] > 0:
-				u.add(ui.Button(f"Sell {mat[0]} (you have {itemhelpers.getItem(mat[1])})").addclick(sellfunc(mat[1])))
+			matKey = mat[1]
+			displayName = mat[0]
+			if itemhelpers.getItems()[matKey] > 0:
+				u.add(ui.Button(f"Sell {displayName} (you have {itemhelpers.getItem(matKey)})").addclick(sellfunc(matKey)))
+		# Buying
+		buyfunc = lambda mat: (lambda: itemhelpers.setMoney(itemhelpers.getMoney() - 1) or itemhelpers.gainItem(mat))
+		for mat in [["Wood", "wood"], ["Leaves", "leaves"], ["Apples", "apple"], ["Berries", "berry"]]:
+			matKey = mat[1]
+			displayName = mat[0]
+			if itemhelpers.getMoney() >= 1:
+				u.add(ui.Button(f"Buy {displayName} (you have {itemhelpers.getItem(matKey)})").addclick(buyfunc(matKey)))
 		ui.render_ui(u)
 	except Exception as e:
 		u = ui.UI()
